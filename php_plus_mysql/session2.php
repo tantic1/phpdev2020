f6f8f20aa5974301d9e61a8474fc67992c49c514
tantic1 <tatjana.antic@sb.t-com.hr>
2019-12-16 19:42:10 +0100

    1612jos azuriranja

--- php_plus_mysql/session2.php
+++ php_plus_mysql/session2.php
@@ -4,14 +4,15 @@ session_start();
 
 ?>
 
-Dobrodošli u trgovinu <?=$_SESSION['moja_var']?> vaš trenutni iznos na računu je <br>
+Dobrodošli u trgovinu <?=$_SESSION['moja_var']?> vaš trenutni oznos na racunu je<br>
 <?=$_SESSION['saldo']?>.
 
-Želite li kupiti ugly sweather (9.99)? <a href="session3.php">DA</a>
+Želite li kupiti ugly sweather ($9.99)? 
 
-<form method="POST" action="session3.php">
-    Komada: <input type="number" name="komada" value="0" max="5" min="0"><br>
+<form method="POST" action="session3.php" >
+    Komada: <input name="komada" type="number" value="0" max="5" min="0"><br>
     <input type="hidden" name="cijena" value="9.99">
     <input type="submit" name="kupi_btn" value="kupi">
 </form>
+
 
